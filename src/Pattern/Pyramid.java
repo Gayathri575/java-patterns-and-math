c575c7b58172afacbf2bdfa2d3e953c7a112fafc
Gayathri575 <gayathriii575@gmail.com>
2025-09-24 19:17:16 +0530

    Pyramid

---
+++ src/Pattern/Pyramid.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class Pyramid {
+    public static void main(String [] args){
+        int num = 4;
+        for (int i = 1 ;i <= num ; i++) {
+            for (int j = 1; j <= num-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
