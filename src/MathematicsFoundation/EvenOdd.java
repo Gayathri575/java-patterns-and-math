5461d515f0cfc2a4f0f30791dfaa1e1d69f65bcb
Gayathri575 <gayathriii575@gmail.com>
2025-09-27 23:07:30 +0530

    Even or Odd

--- src/MathematicsFoundation/EvenOdd.java
+++ src/MathematicsFoundation/EvenOdd.java
@@ -1,4 +1,13 @@
 package MathematicsFoundation;
 
 public class EvenOdd {
+    public static void main (String [] args){
+        int n = 7;
+        if(n % 2 == 0) {
+            System.out.println(n + " is Even");
+        } else {
+            System.out.println(n + " is Odd");
+        }
+
+    }
 }
