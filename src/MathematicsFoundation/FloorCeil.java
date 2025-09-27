ec477ed37c688ef448230cba8adb42c8ca67c6e9
Gayathri575 <gayathriii575@gmail.com>
2025-09-27 23:08:25 +0530

    FloorCeil

--- src/MathematicsFoundation/FloorCeil.java
+++ src/MathematicsFoundation/FloorCeil.java
@@ -1,4 +1,9 @@
 package MathematicsFoundation;
 
 public class FloorCeil {
+    public static void main(String [] args){
+        double num = 3.7;
+        System.out.println("Floor: " + Math.floor(num));
+        System.out.println("Ceil: " + Math.ceil(num));
+    }
 }
