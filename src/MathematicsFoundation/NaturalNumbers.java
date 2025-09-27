9ec93113055d031db11c5d1ddbaca7336e109845
Gayathri575 <gayathriii575@gmail.com>
2025-09-27 23:09:21 +0530

    natural numbers

--- src/MathematicsFoundation/NaturalNumbers.java
+++ src/MathematicsFoundation/NaturalNumbers.java
@@ -1,4 +1,9 @@
 package MathematicsFoundation;
 
 public class NaturalNumbers {
+    public static void main(String [] args){
+        int n = 5;
+        int sum = n * (n + 1) / 2;
+        System.out.println("Sum of first " + n + " numbers: " + sum);
+    }
 }
