9aed65f5333512cb1c1408105cb21153b64e93c3
Gayathri575 <gayathriii575@gmail.com>
2025-09-27 23:12:20 +0530

    Least Common Multiple

--- src/MathematicsFoundation/LCM.java
+++ src/MathematicsFoundation/LCM.java
@@ -1,4 +1,18 @@
 package MathematicsFoundation;
 
 public class LCM {
+    public static void main(String [] args){
+        int a = 4, b = 6;
+        int gcd = 1;
+        int x = a, y = b;
+        while(y != 0){
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        gcd = x;
+        int lcm = (a * b) / gcd;
+        System.out.println("LCM is " + lcm);
+
+    }
 }
